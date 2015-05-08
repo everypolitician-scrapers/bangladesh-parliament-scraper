87928847a8f6f64b15f50a7a9763187327bbabf5
Tony Bowden <tony@tmtm.com>
2015-05-08 08:05:38 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,52 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'date'
+require 'open-uri'
+
+require 'open-uri/cached'
+require 'colorize'
+require 'pry'
+require 'csv'
+
+def noko(url)
+  Nokogiri::HTML(open(url).read) 
+end
+
+@BASE = 'http://www.parliament.gov.bd'
+@PREF = '/index.php/en/mps/members-of-parliament/'
+@terms = {
+  '10' => "current-mp-s/list-of-10th-parliament-members-english",
+  # '9' => "former-mp-s/list-of-9th-parliament-members-english",
+}
+
+@terms.each do |term, pagename|
+  url = @BASE + @PREF + pagename
+  page = noko(url)
+  added = 0
+
+  page.xpath('//table//tr').drop(1).each do |tr|
+    tds = tr.xpath('td')
+    next if tds[1].text.strip == 'Vacant'
+    data = { 
+      localid: tds[0].text.gsub(/[[:space:]]/, ' ').strip,
+      id: tds[3].xpath('.//img/@src').text.split('/').last.split('.').first,
+      name: tds[1].text.gsub(/[[:space:]]/, ' ').strip,
+      constituency: tds[2].text.gsub(/[[:space:]]/, ' ').strip.split('-').first,
+      website: tds[3].xpath('a/@href').text,
+      photograph: tds[3].xpath('a/img/@src').text,
+      party: tds[1].text.gsub(/[[:space:]]/, ' ').strip,
+      source: url,
+      term: term,
+    }
+    # binding.pry if data[:localid][/347/]
+    data[:website].prepend @BASE unless data[:website].empty?
+    puts data.values.to_csv
+    added += 1
+    # ScraperWiki.save_sqlite([:name, :term], data)
+  end
+  warn "Added #{added} for #{term}"
+end
+
