4cc856f78e507c124a9cce4dd2a771cfeec2eeac
Tony Bowden <tony@tmtm.com>
2017-01-23 12:05:33 +0000

    Port to Scraped

--- scraper.rb
+++ scraper.rb
@@ -10,36 +10,68 @@ require 'scraperwiki'
 # OpenURI::Cache.cache_path = '.cache'
 require 'scraped_page_archive/open-uri'
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read)
-end
+class MembersPage < Scraped::HTML
+  decorator Scraped::Response::Decorator::AbsoluteUrls
 
-def scrape_term(url)
-  page = noko_for(url)
-  added = 0
-
-  page.xpath('//table//tr').drop(1).each do |tr|
-    tds = tr.xpath('td')
-    next if %w(vacant -vacant- ---).include?(tds[1].text.tidy.downcase)
-    data = {
-      seatid:       tds[0].text.tidy,
-      name:         tds[1].text.tidy,
-      constituency: tds[2].text.tidy.split('-').first,
-      website:      tds[3].xpath('a/@href').text,
-      photograph:   tds[3].xpath('a/img/@src').text,
-      party:        tds[4].text.tidy,
-      term:         10,
-      source:       url.to_s,
-    }
-    data[:id] = tds[3].xpath('.//img/@src').text.split('/').last.split('.').first rescue ''
-    data[:photograph] = URI.join(url, URI.escape(data[:photograph])).to_s unless data[:photograph].to_s.empty?
-    data[:website] = URI.join(url, URI.escape(data[:website])).to_s unless data[:website].to_s.empty?
-    added += 1
-    # puts data
-    ScraperWiki.save_sqlite(%i(seatid name), data)
+  field :members do
+    noko.xpath('//table//tr').drop(1).map do |tr|
+      fragment tr => MemberRow
+    end
   end
-  warn "Added #{added} members"
 end
+
+class MemberRow < Scraped::HTML
+  field :id do
+    File.basename(photograph, '.jpg')
+  end
+
+  field :seatid do
+    tds[0].text.tidy
+  end
+
+  field :name do
+    tds[1].text.tidy
+  end
+
+  field :constituency do
+    tds[2].text.tidy.split('-').first
+  end
+
+  field :website do
+    tds[3].xpath('a/@href').text
+  end
+
+  field :photograph do
+    tds[3].xpath('a/img/@src').text
+  end
+
+  field :party do
+    tds[4].text.tidy
+  end
+
+  field :term do
+    10
+  end
+
+  field :source do
+    url.to_s
+  end
+
+  def vacant?
+    name.downcase.include?('vacant') || constituency.downcase.include?('vacant')
+  end
+
+  private
+
+  def tds
+    noko.xpath('td')
+  end
+end
+
+start = 'http://www.parliament.gov.bd/index.php/en/mps/members-of-parliament/current-mp-s/list-of-10th-parliament-members-english'
+data = MembersPage.new(response: Scraped::Request.new(url: start).response).members.reject(&:vacant?).map(&:to_h)
+# data.each { |r| puts r.reject { |k, v| v.to_s.empty? }.sort_by { |k, v| k }.to_h }
 
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
-scrape_term 'http://www.parliament.gov.bd/index.php/en/mps/members-of-parliament/current-mp-s/list-of-10th-parliament-members-english'
+ScraperWiki.save_sqlite(%i(seatid name), data)
+warn "Added #{data.count} members"
