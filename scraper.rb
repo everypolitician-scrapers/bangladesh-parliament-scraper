8ff717c9b521bf5dea5b9812b216c316abedc5d4
Oliver Denman <oliver@therewillbeanimals.com>
2017-02-16 16:32:05 +0000

    Move MembersPage to separate file

--- scraper.rb
+++ scraper.rb
@@ -13,16 +13,6 @@ require_rel 'lib'
 # OpenURI::Cache.cache_path = '.cache'
 require 'scraped_page_archive/open-uri'
 
-class MembersPage < Scraped::HTML
-  decorator Scraped::Response::Decorator::AbsoluteUrls
-
-  field :members do
-    noko.xpath('//table//tr').drop(1).map do |tr|
-      fragment tr => MemberRow
-    end
-  end
-end
-
 start = 'http://www.parliament.gov.bd/index.php/en/mps/members-of-parliament/current-mp-s/list-of-10th-parliament-members-english'
 data = MembersPage.new(response: Scraped::Request.new(url: start).response).members.reject(&:vacant?).map(&:to_h)
 # data.each { |r| puts r.reject { |k, v| v.to_s.empty? }.sort_by { |k, v| k }.to_h }
