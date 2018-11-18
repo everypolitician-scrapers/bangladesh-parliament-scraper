b9d5b01738d6e658503094e3c1fdd9dfabd410cb
Tony Bowden <tony@tmtm.com>
2018-11-18 10:53:37 +0000

    add debugging output

--- scraper.rb
+++ scraper.rb
@@ -14,7 +14,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 start = 'http://www.parliament.gov.bd/index.php/en/mps/members-of-parliament/current-mp-s/list-of-10th-parliament-members-english'
 data = MembersPage.new(response: Scraped::Request.new(url: start).response).members.reject(&:vacant?).map(&:to_h)
-# data.each { |r| puts r.reject { |k, v| v.to_s.empty? }.sort_by { |k, v| k }.to_h }
+data.each { |r| puts r.reject { |k, v| v.to_s.empty? }.sort_by { |k, v| k }.to_h } if ENV['MORPH_DEBUG']
 
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
 ScraperWiki.save_sqlite(%i[seatid name], data)
